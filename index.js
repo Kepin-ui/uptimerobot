d398d2dfa8a030a6e200adf8446e149d9c8569a8
Kepin-ui <68527260+Kepin-ui@users.noreply.github.com>
2020-09-19 13:14:49 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const axios = require("axios")
-const urls = ["https://profuse-cerulean-swordfish.glitch.me"]
+const urls = ["https://friend-last-pr.glitch.me"]
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
